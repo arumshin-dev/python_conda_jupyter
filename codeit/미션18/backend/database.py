b16d1d02fedd4ee82b5b9c5679dfb4bafb0e0cd6
arumshin-dev <arumshin.dev@gmail.com>
2026-01-23 00:04:08 +0900

    -

--- codeit/미션18/backend/database.py
+++ codeit/미션18/backend/database.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DB 연결
-SQLALCHEMY_DATABASE_URL = "sqlite:///./movies.db"  # 예시: SQLite 사용
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# sqlite 데이터베이스 연결
+SQLALCHEMY_DATABASE_URL = "sqlite:///./movies.db"
 
-# Base 클래스 선언
-Base = declarative_base()
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
-# 세션 생성
+# 데이터베이스 세션 생성
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 데이터베이스 모델 생성
+Base = declarative_base()
